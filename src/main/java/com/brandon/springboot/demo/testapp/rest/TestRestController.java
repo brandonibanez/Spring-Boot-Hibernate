3fc41573cdeb06ddc3a426798b0fe5a77ab29d64
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-12-02 19:09:40 +0800

    Reading Values From Property File

--- src/main/java/com/brandon/springboot/demo/testapp/rest/TestRestController.java
+++ src/main/java/com/brandon/springboot/demo/testapp/rest/TestRestController.java
@@ -1,14 +1,18 @@
 package com.brandon.springboot.demo.testapp.rest;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class TestRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
     
     @GetMapping("")
     public String sayHello() {
-        return "Hello World";
+        return coachName;
     }
 }
