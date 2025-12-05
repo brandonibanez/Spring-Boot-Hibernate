f7be23715d6c6162a485900c870ccb86aa36a756
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-12-05 22:14:35 +0800

    Section 3
    Hibernate/JPA CRUD

--- src/main/java/com/brandon/util/Coach.java
+++
@@ -1,6 +0,0 @@
-package com.brandon.util;
-
-
-public interface Coach {
-    String getDailyWorkout();
-}
--- src/test/java/com/brandon/cruddemo/CruddemoApplicationTests.java
+++ src/test/java/com/brandon/cruddemo/CruddemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.brandon.springcoredemo;
+package com.brandon.cruddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringcoredemoApplicationTests {
+class CruddemoApplicationTests {
 
 	@Test
 	void contextLoads() {
