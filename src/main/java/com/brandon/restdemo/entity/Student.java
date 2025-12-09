c8ee911ce58bdb3a232b7d051bb198e26fc199f5
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-12-09 20:08:17 +0800

    Spring Boot REST POJO

---
+++ src/main/java/com/brandon/restdemo/entity/Student.java
@@ -0,0 +1,37 @@
+package com.brandon.restdemo.entity;
+
+public class Student {
+
+    String firstName;
+    String lastName;
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+}
--- src/main/java/com/brandon/restdemo/rest/DemoRestController.java
+++ src/main/java/com/brandon/restdemo/rest/DemoRestController.java
@@ -3,6 +3,15 @@ package com.brandon.restdemo.rest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brandon.restdemo.entity.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @RestController
 @RequestMapping("/api")
 public class DemoRestController {
@@ -11,5 +20,14 @@ public class DemoRestController {
     public String sayHello() {
         return "Hello World!";
     }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Brandon", "Smith"));
+        students.add(new Student("Mary", "Public"));
+        return students;
+    }
+    
 
 }
