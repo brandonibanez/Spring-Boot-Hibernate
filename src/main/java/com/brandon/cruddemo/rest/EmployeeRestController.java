4f6869461a924e877c0217b9b0d4ee5789402f74
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-12-10 19:09:49 +0800

    Rest Controller - Get Single Employee

--- src/main/java/com/brandon/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/brandon/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.brandon.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brandon.cruddemo.entity.Employee;
@@ -20,6 +21,11 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+    
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        return employeeService.findById(id);
     }
 
 }
