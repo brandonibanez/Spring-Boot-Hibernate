d0ecdeaf407725a520dcef966dc5be798593be73
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-12-16 22:23:59 +0800

    Spring Security - JDBC Authentication

--- src/main/java/com/brandon/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/brandon/cruddemo/security/DemoSecurityConfig.java
@@ -2,6 +2,8 @@ package com.brandon.cruddemo.security;
 
 import java.security.Security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,33 +13,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
         
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-        
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
+        return new JdbcUserDetailsManager(dataSource);
 
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
     }   
 
     @Bean
