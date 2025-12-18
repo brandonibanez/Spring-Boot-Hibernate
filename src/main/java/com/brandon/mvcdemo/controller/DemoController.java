032e4ddea93a8ade68f499c3e0ec36d21476c4ce
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-12-18 20:49:54 +0800

    Spring MVC Form Data Binding

--- src/main/java/com/brandon/mvcdemo/controller/DemoController.java
+++ src/main/java/com/brandon/mvcdemo/controller/DemoController.java
@@ -1,10 +1,16 @@
 package com.brandon.mvcdemo.controller;
 
+import javax.xml.transform.stax.StAXResult;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.brandon.mvcdemo.model.Student;
 
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,19 +26,32 @@ public class DemoController {
 
     // }
 
-    @RequestMapping("/showForm")
-    public String showForm() {
+    @GetMapping("/showForm")
+    public String showForm(Model model) {
+
+        Student student = new Student();
+        student.setFirstName("John");
+        student.setLastName("Doe");
+        model.addAttribute("student", student);
+
         return "helloworld-form";
     }
 
-    @RequestMapping("/processForm")
-    public String processForm(@RequestParam("userName") String userName, Model model) {
+    // @RequestMapping("/processForm")
+    // public String processForm(@RequestParam("userName") String userName, Model model) {
 
-        // String userName = request.getParameter("userName");
-        userName = userName.toUpperCase();
-        model.addAttribute("message", userName);
+    //     // String userName = request.getParameter("userName");
+    //     userName = userName.toUpperCase();
+    //     model.addAttribute("message", userName);
 
-        return "helloworld";
+    //     return "helloworld";
+    // }
+
+    @PostMapping("/processForm")
+    public String processForm(@ModelAttribute("student") Student student) {
+
+        System.out.println(student.getFirstName());
+        return "helloworld-confirmation";
     }
 
     
