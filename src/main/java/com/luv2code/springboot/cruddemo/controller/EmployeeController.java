6eb5b4a94ef1c3c583d08846966e3c467739245d
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-12-21 21:40:12 +0800

    Delete Employee Feature

--- src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -60,6 +61,13 @@ public class EmployeeController {
     public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
         
         employeeService.save(theEmployee);
+
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/delete")
+    public String deleteEmployee(@RequestParam("id") int theId) {
+        employeeService.deleteById(theId);
 
         return "redirect:/employees/list";
     }
