b0e5c01c5ca03d29937c064fbe8b9e3a6c5c0bed
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-12-21 21:04:32 +0800

    Section 7 Spring MVC CRUD (Starter FIles)

--- src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
@@ -1,13 +1,13 @@
-package com.brandon;
+package com.luv2code.springboot.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MvcvalidationdemoApplication {
+public class CruddemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MvcvalidationdemoApplication.class, args);
+		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
 }
--- src/test/java/com/luv2code/springboot/cruddemo/CruddemoApplicationTests.java
+++ src/test/java/com/luv2code/springboot/cruddemo/CruddemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.brandon;
+package com.luv2code.springboot.cruddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MvcvalidationdemoApplicationTests {
+class CruddemoApplicationTests {
 
 	@Test
 	void contextLoads() {
