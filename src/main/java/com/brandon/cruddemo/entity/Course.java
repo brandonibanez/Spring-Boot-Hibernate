e29e3f2bd8b343209b743240a57aaad19d05747d
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-12-30 14:17:00 +0800

    Annotate Course Entity

--- src/main/java/com/brandon/cruddemo/entity/Course.java
+++ src/main/java/com/brandon/cruddemo/entity/Course.java
@@ -1,14 +1,30 @@
 package com.brandon.cruddemo.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="course")
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private int id;
+
+    @Column(name="title")
     private String title;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+                          CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name="instructor_id")
     private Instructor instructor;
 
     public Course() {
