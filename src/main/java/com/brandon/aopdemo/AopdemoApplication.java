2dc173b6e94d0c4f9b2f462e4d589bf6c2714be1
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-01 19:27:57 +0800

    Creating Java Classes

--- src/main/java/com/brandon/aopdemo/AopdemoApplication.java
+++ src/main/java/com/brandon/aopdemo/AopdemoApplication.java
@@ -1,13 +1,29 @@
 package com.brandon.aopdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.brandon.aopdemo.dao.AccountDAO;
 
 @SpringBootApplication
 public class AopdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO accountDAO) {
+		return args -> {
+			demoTheBeforeAdvice(accountDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO accountDAO) {
+		System.out.println("\nMain Program: demoTheBeforeAdvice");
+		accountDAO.addAccount();
 	}
 
 }
