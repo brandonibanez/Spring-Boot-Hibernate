5d2193117935ba3e58e5869e8c9eefcdc1096f45
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-01-02 14:27:07 +0800

    Pointcut Declarations

--- src/main/java/com/brandon/aopdemo/aspect/LoggingAspectTime.java
+++ src/main/java/com/brandon/aopdemo/aspect/LoggingAspectTime.java
@@ -2,13 +2,17 @@ package com.brandon.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspectTime {
 
-    @Before("execution(void addAccount*(String))")
+    @Pointcut("execution(* com.brandon.aopdemo.dao.*.*(..))")
+    public void daoPackage() {}
+
+    @Before("daoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on addAccount()");
     }
